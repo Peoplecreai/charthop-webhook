530af5ce6b44a5245b690048cf0b2fafe43b716a
Peoplecreai <rh@creai.mx>
2025-09-26 22:30:10 -0600

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility package for legacy import paths.
-
-This package re-exports handlers implemented under the :mod:`app.handlers`
-namespace so existing integrations that import from ``handlers`` continue to
-work without modification.
-"""
-from app.handlers.runn_to_bq import export_handler as runn_export_handler
-
-__all__ = ["runn_export_handler"]
