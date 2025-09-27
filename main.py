cd30e855569130d5055136f3bef5c073bbc2156c
Peoplecreai <rh@creai.mx>
2025-09-26 21:33:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from app.main import app  # expone la Flask app para gunicorn
+from app import create_app
+
+# WSGI entrypoint
+app = create_app()
+
+if __name__ == "__main__":
+    # útil para correr local
+    app.run(host="0.0.0.0", port=8080, debug=False)
